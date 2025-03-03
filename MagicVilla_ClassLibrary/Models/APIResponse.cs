504395253869a4adf50ae8e1aeeb12af7e205619
Teeradet <tee2490@hotmail.com>
2025-03-03 11:45:21 +0700

    Section 6 – 4 API Response

---
+++ MagicVilla_ClassLibrary/Models/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace MagicVilla_ClassLibrary.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+    }
+}
