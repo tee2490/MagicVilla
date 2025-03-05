96edefbb4b865c7b3b5cb2a654af7269d918b564
Teeradet <tee2490@hotmail.com>
2025-03-05 11:09:42 +0700

    Section 8 – 4 User Controller API

--- MagicVilla_ClassLibrary/Models/APIResponse.cs
+++ MagicVilla_ClassLibrary/Models/APIResponse.cs
@@ -4,6 +4,11 @@ namespace MagicVilla_ClassLibrary.Models
 {
     public class APIResponse
     {
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
