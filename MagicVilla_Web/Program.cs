0924e3c73aef3911cea2f00a0dfe7eccb7d9dc5a
Teeradet <tee2490@hotmail.com>
2025-03-05 16:35:02 +0700

    Section 9 – 5 Web NavBar Display

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -23,6 +23,8 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
