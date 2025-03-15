c18bd807259c993474dfc5ad03c615ad3ee63fcb
Teeradet <tee2490@hotmail.com>
2025-03-15 15:22:06 +0700

    Section 2-3 Add Controller API – HTTPGET

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_ClassLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+             new Villa{Id=1,Name="Pool View" },
+             new Villa{Id=2,Name="Beach View" }
+             };
+        }
+    }
+}
