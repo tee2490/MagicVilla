d56c65fffdf49d064f60ff8ea260a6bed7d9d41f
Teeradet <tee2490@hotmail.com>
2025-03-16 15:27:16 +0700

    Section 4-4 Create DbContext & Migration

--- MagicVilla_ClassLibrary/Models/Dto/VillaDTO.cs
+++ MagicVilla_ClassLibrary/Models/Dto/VillaDTO.cs
@@ -5,11 +5,15 @@ namespace MagicVilla_ClassLibrary.Models.Dto
     public class VillaDTO
     {
         public int Id { get; set; }
-
         [Required]
-        [MinLength(3), MaxLength(30)]
+        [MaxLength(30)]
         public string Name { get; set; }
+        public string? Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Amenity { get; set; }
     }
 }
---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=Teeradet;Database=TestMagicVilla;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+
+    }
+}
