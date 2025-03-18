e1911cabeb5eb3a0b4097a861ba4160dc5118ac6
Teeradet <tee2490@hotmail.com>
2025-03-18 16:00:19 +0700

    Section 6-6 CRUD VillaNumber & Implement Repository

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 builder.Services.AddScoped<IVillaRepository, VillaRepository>();
+builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 
 var app = builder.Build();
 
---
+++ MagicVilla_VillaAPI/Repository/IRepostiory/IVillaNumberRepository.cs
@@ -0,0 +1,9 @@
+﻿using MagicVilla_ClassLibrary.Models;
+
+namespace MagicVilla_VillaAPI.Repository.IRepostiory
+{
+    public interface IVillaNumberRepository : IRepository<VillaNumber>
+    {
+        Task<VillaNumber> UpdateAsync(VillaNumber entity);
+    }
+}
