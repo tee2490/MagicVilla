969ae31970703fe2de8236d04fa1ccacd9ef2dd7
Teeradet <tee2490@hotmail.com>
2025-03-19 09:15:16 +0700

    Section 7-3 AutoMapper & Add API URL in AppSettings

---
+++ MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla_ClassLibrary.Models.Dto;
+
+namespace MagicVilla_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
