46561e41cfab5284530df738c236037699df4c79
Teeradet <tee2490@hotmail.com>
2025-03-21 14:41:42 +0700

    Section 12-4 How to handle FormFile in BaseService

--- MagicVilla_Web/Services/BaseService.cs
+++ MagicVilla_Web/Services/BaseService.cs
@@ -24,14 +24,49 @@ namespace MagicVilla_Web.Services
             {
                 var client = httpClient.CreateClient("MagicAPI");
                 HttpRequestMessage message = new HttpRequestMessage();
-                message.Headers.Add("Accept", "application/json");
+                if (apiRequest.ContentType == SD.ContentType.MultipartFormData)
+                {
+                    message.Headers.Add("Accept", "*/*");
+                }
+                else
+                {
+                    message.Headers.Add("Accept", "application/json");
+                }
                 message.RequestUri = new Uri(apiRequest.Url);
 
-                if (apiRequest.Data != null)
+                //ตรวจสอบการแนบไฟล์
+                if (apiRequest.ContentType == SD.ContentType.MultipartFormData)
                 {
-                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
-                        Encoding.UTF8, "application/json");
+                    var content = new MultipartFormDataContent();
+
+                    foreach (var prop in apiRequest.Data.GetType().GetProperties())
+                    {
+                        var value = prop.GetValue(apiRequest.Data);
+                        if (value is FormFile)
+                        {
+                            var file = (FormFile)value;
+                            if (file != null)
+                            {
+                                content.Add(new StreamContent(file.OpenReadStream()), prop.Name, file.FileName);
+                            }
+                        }
+                        else
+                        {
+                            content.Add(new StringContent(value == null ? "" : value.ToString()), prop.Name);
+                        }
+                    }
+
+                    message.Content = content;
                 }
+                else
+                {
+                    if (apiRequest.Data != null)
+                    {
+                        message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                            Encoding.UTF8, "application/json");
+                    }
+                }
+
 
                 switch (apiRequest.ApiType)
                 {
