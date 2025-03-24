fcd5d3922969abb1f0cec4ce8cd18c57234a65ac
Teeradet <tee2490@hotmail.com>
2025-03-24 08:27:35 +0700

    Section 14-5 Web : Add Refresh Token to Token Provider

--- MagicVilla_Web/Services/TokenProvider.cs
+++ MagicVilla_Web/Services/TokenProvider.cs
@@ -16,7 +16,7 @@ namespace MagicVilla_Web.Services
 
         public void ClearToken()
         {
-            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.AccessToken);
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.RefreshToken);
         }
 
         public TokenDTO GetToken()
@@ -24,9 +24,11 @@ namespace MagicVilla_Web.Services
             try
             {
                 bool hasAccessToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.AccessToken, out string accessToken);
+                bool hasRefreshToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.RefreshToken, out string refreshToken);
                 TokenDTO tokenDTO = new()
                 {
-                    AccessToken = accessToken
+                    AccessToken = accessToken,
+                    RefreshToken = refreshToken
                 };
                 return hasAccessToken ? tokenDTO : null;
             }
@@ -39,7 +41,7 @@ namespace MagicVilla_Web.Services
         public void SetToken(TokenDTO tokenDTO)
         {
             var cookieOptions = new CookieOptions { Expires = DateTime.UtcNow.AddDays(60) };
-            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.AccessToken, tokenDTO.AccessToken, cookieOptions);
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.RefreshToken, tokenDTO.RefreshToken, cookieOptions);
         }
     }
 }
